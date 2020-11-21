19be533d2e26a5585049ca3b90d8939647e56af2
Davi Nunes <davinunesg@gmail.com>
2020-11-21 12:01:03 -0300

    insert function

--- apiVeiculos/app/src/main/java/com/example/apiveiculos/MainActivity.kt
+++ apiVeiculos/app/src/main/java/com/example/apiveiculos/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.apiveiculos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import model.Veiculo
 import com.android.volley.Request
 import com.android.volley.Response
@@ -25,6 +29,25 @@ class MainActivity : AppCompatActivity() {
         listViewVeiculo = findViewById(R.id.listViewVeiculo)
         arrayListViewVeiculo = ArrayList()
         carregarDadosVeiculos()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = MenuInflater(this)
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.adicionarButton->{
+                veiculo = Veiculo("dav-1234", "Davi", "Nunes", 1850, 17.0)
+                insert(veiculo)
+                Toast.makeText(this, veiculo.toString(), Toast.LENGTH_LONG).show()
+                carregarDadosVeiculos()
+            }
+        }
+        return super.onOptionsItemSelected(item)
 
     }
 
@@ -57,5 +80,23 @@ class MainActivity : AppCompatActivity() {
         val requestQueue = Volley.newRequestQueue(this)
         requestQueue.add<String>(stringRequest)
     }
+
+    fun insert(veiculo: Veiculo){
+        val url = "https://aulahttpphpmysql.000webhostapp.com/webservice/addVeiculo.php?" +
+                "HTTP_PLACA=${veiculo.placa}&" +
+                "HTTP_MARCA=${veiculo.marca}&" +
+                "HTTP_MODELO=${veiculo.modelo}&" +
+                "HTTP_ANO=${veiculo.ano}&" +
+                "HTTP_MOTOR=${veiculo.motor}"
+        val stringRequest = StringRequest(
+                Request.Method.GET,
+                url,
+                Response.Listener { s->
+                },
+                Response.ErrorListener { VolleyError ->  })
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add<String>(stringRequest)
+    }
+
 
 }
